bbd7437810ee30997105b1b8e98ea5e4849587fb
brmdv <bramdevroey@gmail.com>
2021-01-27 15:41:30 +0100

    Updated immoscraping

--- scrapers/immo_scraping.py
+++ scrapers/immo_scraping.py
@@ -1,28 +1,34 @@
-from ..property_structure import Property
+import requests
+
+from property_structure import Property
 
 
 class ImmoScraper:
-    """Base class for an immo scraper. All other scrapers should inherite from this one."""
+    """Base class for an immo scraper. All other scrapers should inherit from this one."""
 
-    def __init__(self, page_url):
+    def __init__(self, page_url: str):
         self.page_url = page_url
+
         # Download to html file
         self._download_page()
 
     def _download_page(self):
         """Downloads the page for internal purposes."""
-        pass
+        response = requests.get(self.page_url)
+        self._data = response.content
 
 
 class ImmoPageScraper(ImmoScraper):
     """Gets all information for a certain house/appartment."""
 
-    def __init__(self, page_url):
+    def __init__(self, page_url: str):
         super().__init__(page_url)
         self._property = Property()
 
     def scrape(self):
-        """Reimplement this method for every scraper."""
+        """Reimplement this method for every scraper. It should get all needed
+        information from self._data and save it in self._property
+        """
         pass
 
 
