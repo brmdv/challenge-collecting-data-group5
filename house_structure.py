de1ec45fa011866f17d99324d667c72b558c95af
brmdv <bramdevroey@gmail.com>
2021-01-27 11:42:35 +0100

    Created basic House class

---
+++ house_structure.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class House:
+    """Dataclass that represents a property.
+    """
+
+    locality: str = None
+    property_type: str = None
+    property_subtype: str = None
+    price: int = None
+    sale_type: str = None
+    number_rooms: int = None
+    area: float = None
+    fully_equipped_kitchen: bool = None
+    is_furnished: bool = None
+    has_open_fire: bool = None
+    has_terrace: bool = None
+    has_garden: bool = None
+    land_surface: float = None
+    land_plot_area: float = None
+    number_facades: int = None
+    has_swimming_pool: bool = None
+    building_state: str = None
+
+    def __str__(self) -> str:
+        return "House"
+
+    def csv(self):
+        pass
