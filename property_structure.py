ac4f64357e9ed94c3be19cdd878eedfb634c0fd7
Bram De vroey <bramdevroey@gmail.com>
2021-01-28 14:54:36 +0100

    Property and general scraping

--- property_structure.py
+++ property_structure.py
@@ -3,13 +3,12 @@ from dataclasses import dataclass
 
 @dataclass
 class Property:
-    """Dataclass that represents a property.
-    """
+    """Dataclass that represents a property."""
 
     locality: str = None
     property_type: str = None
     property_subtype: str = None
-    price: int = None
+    price: float = None
     sale_type: str = None
     number_rooms: int = None
     area: float = None
@@ -23,6 +22,14 @@ class Property:
     number_facades: int = None
     has_swimming_pool: bool = None
     building_state: str = None
+
+    # Counter to keep set id
+    _count: int = 0
+
+    def __post_init__(self):
+        """Everything that needs to happen after init. Sets id and validates."""
+        self.id = Property._count
+        Property._count += 1
 
     def __str__(self) -> str:
         return "Property" + f"({self.property_type})" if self.property_type else ""
